fa278015c63b8a7673aac1c0777a7642f821752a
thelonewolf123 <harishkumar.vellore@gmail.com>
2025-12-30 18:02:20 +0530

    feat: use open router

--- backend/agent/openai_agent.py
+++ backend/agent/openai_agent.py
@@ -1,13 +1,19 @@
 from typing import List
 from langchain.agents import create_agent
 from langchain.messages import HumanMessage, AIMessage
-
+from langchain_openai import ChatOpenAI
 from tools.weather import get_weather
+import os
 
 
 def get_openai_agent():
+    llm = ChatOpenAI(
+        model="x-ai/grok-4-fast",
+        api_key=os.environ.get("OPENAI_API_KEY"),
+        base_url="https://openrouter.ai/api/v1/",
+    )
     agent = create_agent(
-        "gpt-4.1",
+        llm,
         tools=[get_weather],
         system_prompt="You are a helpful assistant",
     )
